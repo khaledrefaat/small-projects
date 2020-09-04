98682983a44a45a911bf7f6dd058dabcae4144d5
unknown <khaledrefaat08@gmail.com>
2020-09-04 19:45:31 +0200

    replace then with async await

--- dog-slideshow/app.js
+++ dog-slideshow/app.js
@@ -1,6 +1,7 @@
 const img = document.querySelector('.img');
 const sel = document.querySelector('.dog-selector');
 let dog = sel.value.replace(/'-'/gm, '/');
+let regex = /-/gm;
 fetch(`https://dog.ceo/api/breed/${dog}/images/random`)
 	.then((res) => res.json())
 	.then((data) => {
@@ -11,21 +12,28 @@ fetch(`https://dog.ceo/api/breed/${dog}/images/random`)
 		console.log(err);
 	});
 
-function randomDog() {
-	let regex = /-/gm;
-	// when the dog names contains 2 words like(english bulldog) the api puts(english-bulldog) we are replacing the(-)
-	let dogRand = sel.value.replace(regex, '/');
+// function randomDog() {
+// 	// when the dog names contains 2 words like(english bulldog) the api puts(english-bulldog) we are replacing the(-)
+// 	let dogRand = sel.value.replace(regex, '/');
 
-	fetch(`https://dog.ceo/api/breed/${dogRand}/images/random`)
-		.then((res) => res.json())
-		.then((data) => {
-			console.log(data);
-			img.src = data.message;
-		})
-		.catch((err) => {
-			console.log(err);
-		});
-	// console.log(dog);
+// 	fetch(`https://dog.ceo/api/breed/${dogRand}/images/random`)
+// 		.then((res) => res.json())
+// 		.then((data) => {
+// 			console.log(data);
+// 			img.src = data.message;
+// 		})
+// 		.catch((err) => {
+// 			console.log(err);
+// 		});
+// 	// console.log(dog);
+// }
+
+async function randomDog() {
+	let dogRand = sel.value.replace(regex, '/');
+	const response = await fetch(`https://dog.ceo/api/breed/${dogRand}/images/random`);
+	const data = await response.json();
+	console.log(data);
+	img.src = data.message;
 }
 
 setInterval(randomDog, 3000);
