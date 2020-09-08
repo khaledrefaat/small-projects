b2a7103b5543c843119d64ca0439f07cef422f72
unknown <khaledrefaat08@gmail.com>
2020-09-08 21:42:02 +0200

    the dog-slideshow works good with no errors

--- dog-slideshow/app.js
+++ dog-slideshow/app.js
@@ -1,11 +1,17 @@
 const img_div = document.querySelector('.img-container');
+const img_img = document.querySelector('.slide');
 const select_div = document.querySelector('.selector-container');
+let timer, deleteFirstPhotoDelay;
 
 async function start() {
-	const response = await fetch(`https://dog.ceo/api/breeds/list/all`);
-	const data = await response.json();
-	console.log(data);
-	createBreedList(data.message);
+	try {
+		const response = await fetch(`https://dog.ceo/api/breeds/list/all`);
+		const data = await response.json();
+		console.log(data);
+		createBreedList(data.message);
+	} catch (e) {
+		console.log(e);
+	}
 }
 
 start();
@@ -24,12 +30,14 @@ function createBreedList(breedList) {
 }
 
 async function loadByBreed(breed) {
-	breed = breed || 'african';
 	if (breed !== 'Choose A Dog To Breed') {
-		const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`);
-		const data = await response.json();
-		console.log(data);
-		createSlideShow(data.message);
+		try {
+			const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`);
+			const data = await response.json();
+			createSlideShow(data.message);
+		} catch (e) {
+			console.log(e);
+		}
 	}
 }
 
@@ -37,7 +45,33 @@ loadByBreed();
 
 function createSlideShow(images) {
 	let currPosition = 0;
-	img_div.innerHTML = `
-		<div class="slide" style="background-image: url(${images[currPosition]})"></div>
-	`;
+	clearInterval(timer);
+	clearTimeout(deleteFirstPhotoDelay);
+	if (images.length > 1) {
+		img_div.innerHTML = `
+		<div class="slide" style="background-image: url(${images[0]})"></div>
+		<div class="slide" style="background-image: url(${images[1]})"></div>
+		`;
+		currPosition += 2;
+		timer = setInterval(nextSlide, 3000);
+	} else {
+		img_div.innerHTML = `
+		<div class="slide" style="background-image: url(${images[0]})"></div>
+		<div class="slide"></div>
+		`;
+	}
+
+	function nextSlide() {
+		img_div.insertAdjacentHTML(
+			'beforeend',
+			`<div class="slide" style="background-image: url(${images[currPosition]})"></div>`
+		);
+		deleteFirstPhotoDelay = setTimeout(function() {
+			document.querySelector('.slide').remove();
+		}, 1000);
+		console.log(currPosition);
+		console.log('------------------------- Before');
+		currPosition + 1 >= images.length ? (currPosition = 0) : currPosition++;
+		console.log(currPosition);
+	}
 }
